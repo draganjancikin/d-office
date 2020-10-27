df0d9cecf5f9a1bb53db1c8d948401932684f08a
draganjancikin <dragan.jancikin@gmail.com>
2020-10-27 10:03:14 +0100

    add function basicValidation to client add.php

--- src/client/includes/add.php
+++ src/client/includes/add.php
@@ -1,4 +1,7 @@
 <?php
+function basicValidation($str){
+    return trim(htmlspecialchars($str));
+}
 // add client
 if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newClient"])) {
     
@@ -11,10 +14,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newClient"])) {
         $nameError = 'Ime mora biti upisano';
         die('<script>location.href = "?new&name_error" </script>');
 	} else {
-		$name = trim(htmlspecialchars($_POST['name']));
+		$name = basicValidation($_POST['name']);
     }
     
-    $name_note = trim(htmlspecialchars($_POST["name_note"]));
+    $name_note = basicValidation($_POST["name_note"]);
     if(isset($_POST["is_supplier"])) {
         $is_supplier = $_POST["is_supplier"];
     } else {
@@ -23,9 +26,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newClient"])) {
     $state_id = $_POST["state_id"];
     $city_id = $_POST["city_id"];
     $street_id = $_POST["street_id"];
-    $home_number = trim(htmlspecialchars($_POST["home_number"]));
-    $adress_note = trim(htmlspecialchars($_POST["adress_note"]));
-    $note = trim(htmlspecialchars($_POST["note"]));
+    $home_number = basicValidation($_POST["home_number"]);
+    $adress_note = basicValidation($_POST["adress_note"]);
+    $note = basicValidation($_POST["note"]);
     
     $db = new DB();
     $connect_db = $db->connectDB();
@@ -45,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newClient"])) {
             $insert_row = $connect_db->query( " INSERT INTO client (vps_id, name, name_note, is_supplier, state_id, city_id, street_id, home_number, adress_note, note, created_at_date, created_at_user_id) "
                                             . " VALUES ('$vps_id', '$name', '$name_note', '$is_supplier', '$state_id', '$city_id', '$street_id', '$home_number', '$adress_note', '$note', '$date', '$user_id') ") or die(mysqli_error($connect_db));
         } elseif ($vps_id == 2){
-            $lb = htmlspecialchars($_POST["pib"]);
+            $lb = basicValidation($_POST["pib"]);
             //MySqli Insert Query
             $insert_row = $connect_db->query( " INSERT INTO client (vps_id, name, name_note, lb, is_supplier, state_id, city_id, street_id, home_number, adress_note, note, created_at_date, created_at_user_id) "
                                             . " VALUES ('$vps_id', '$name', '$name_note', '$lb', '$is_supplier', '$state_id', '$city_id', '$street_id', '$home_number', '$adress_note', '$note', '$date', '$user_id') ") or die(mysqli_error($connect_db));
@@ -67,8 +70,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newContact"])) {
     
     $client_id = htmlspecialchars($_POST["client_id"]);
     $type_id = htmlspecialchars($_POST["contacttype_id"]);
-    $number = trim(htmlspecialchars($_POST["number"]));
-    $note = trim(htmlspecialchars($_POST["note"]));
+    $number = basicValidation($_POST["number"]);
+    $note = basicValidation($_POST["note"]);
     
     $db = new DB();
     $connection = $db->connectDB();
@@ -84,8 +87,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["newContact"])) {
 // add State, City and Street
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET["addSCS"])) {
 
-    $action = htmlspecialchars($_POST["action"]);
-    $name = trim(htmlspecialchars($_POST["name"]));
+    $action = basicValidation($_POST["action"]);
+    $name = basicValidation($_POST["name"]);
 
     $db = new DB();
     $connect_db = $db->connectDB();
