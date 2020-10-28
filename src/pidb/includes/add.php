3201ace03efb7b7347c1276b41bf368f401e86b8
draganjancikin <dragan.jancikin@gmail.com>
2020-10-28 13:17:24 +0100

    pidb add.php and export.php cleanup and update

--- src/pidb/includes/add.php
+++ src/pidb/includes/add.php
@@ -4,7 +4,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addPidb"]) ) {
 
     $user_id = $_SESSION['user_id'];
     $date = date('Y-m-d h:i:s');
-    
     $client_id = htmlspecialchars($_POST["client_id"]);
     $pidb_tip_id = htmlspecialchars($_POST["pidb_tip_id"]);
 
@@ -13,18 +12,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addPidb"]) ) {
     } else {
         $project_id = 0;
     }
-    
+
     $title = htmlspecialchars($_POST["title"]);
     $note = htmlspecialchars($_POST["note"]);
-    
+
     $db = new DB();
     $connect_db = $db->connectDB();
-    
+
     $connect_db->query("INSERT INTO pidb (tip_id, date, client_id, project_id, title, note) " 
     . " VALUES ('$pidb_tip_id', '$date', '$client_id', '$project_id', '$title', '$note')") or die(mysqli_error($connect_db));
     
     $pidb_id = $connect_db->insert_id;
-    
+
     // if exist project_id write project_id and pidb_id to table project_pidb
     if(isset($_POST["project_id"])){
         $project_id = htmlspecialchars($_POST["project_id"]);
@@ -41,13 +40,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addArticleInPidb"]) ) {
 
     $pidb_id = htmlspecialchars($_POST["pidb_id"]);
     $pidb_tip_id = $pidb->getTipid($pidb_id);
-
     $article_id = htmlspecialchars($_POST["article_id"]);
     $note = htmlspecialchars($_POST["note"]);
     $pieces = htmlspecialchars($_POST["pieces"]);
-
     $price = $article->getPrice($article_id);
-  
     $tax = $pidb->getTax();
 
     $db = new DB();
@@ -62,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addArticleInPidb"]) ) {
     //insert property-a artikla u tabelu pidb_article_property
     $propertys = $connection->query( "SELECT * FROM article_property WHERE article_id ='$article_id'");
     while($row_property = mysqli_fetch_array($propertys)){
-    
+
         $property_id = $row_property['property_id'];
         $quantity = 0;
 
@@ -75,11 +71,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addArticleInPidb"]) ) {
 
 // add payment
 if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addPayment"]) ) {
-    
+
     $date = date('Y-m-d h:i:s');
-
     $pidb_id = htmlspecialchars($_POST["pidb_id"]);
-
     $payment_type_id = htmlspecialchars($_POST["payment_type_id"]);
     $amount = htmlspecialchars($_POST["amount"]);
     $note = htmlspecialchars($_POST["note"]);
@@ -89,23 +83,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addPayment"]) ) {
 
     $connection->query("INSERT INTO payment (date, pidb_id, payment_type_id, amount, note) " 
                     . " VALUES ('$date', '$pidb_id', '$payment_type_id', '$amount', '$note' )") or die(mysqli_error($connection));
-                    
+
     die('<script>location.href = "?view&pidb_id='.$pidb_id.' " </script>');
 }
 
 // duplicate article in document
 if($_SERVER["REQUEST_METHOD"] == "GET" AND isset($_GET["duplicateArticleInPidb"])) {
-    
+
     $pidb_id = htmlspecialchars($_GET["pidb_id"]);
     $pidb_tip_id = htmlspecialchars($_GET["pidb_tip_id"]);
 
     // id in table pidb_article
     $pidb_article_id = htmlspecialchars($_GET["pidb_article_id"]);
-    
+
     // sledeća metoda duplicira artikal iz pidb_article i property-e iz pidb_article_property
     $pidb->duplicateArticleInPidb($pidb_article_id);
-    
+
     die('<script>location.href = "?edit&pidb_id='.$pidb_id.'" </script>');
-    
-  }
-  
+}
--- src/pidb/includes/export.php
+++ src/pidb/includes/export.php
@@ -1,5 +1,5 @@
 <?php
-// export predračuna u otpremnicu
+// export proforma to dispatch
 if(isset($_GET["exportProformaToDispatch"]) ) {
 
     $pidb_id = htmlspecialchars($_GET["pidb_id"]);
@@ -11,22 +11,22 @@ if(isset($_GET["exportProformaToDispatch"]) ) {
     $connection = $db->connectDB();
 
     // get proforma data
-    $result_pidbs = $connection->query("SELECT * FROM pidb WHERE id = '$pidb_id' ") or die(mysqli_error($connection));
-        $row_pidb = mysqli_fetch_array($result_pidbs);
+    $result_pidb = $connection->query("SELECT * FROM pidb WHERE id = '$pidb_id' ") or die(mysqli_error($connection));
+        $row_pidb = $result_pidb->fetch_assoc();
         $client_id = $row_pidb['client_id'];
         $project_id = $row_pidb['project_id'];
         $title = $row_pidb['title'];
         $note = $row_pidb['note'];
-    
+
     // save data to dispatch
     $connection->query("INSERT INTO pidb (tip_id, date, client_id, parent_id, project_id, title, note) VALUES ('$pidb_tip_id', '$date', '$client_id', '$pidb_id', '$project_id', '$title', '$note')") or die(mysqli_error($connection));
-    
+
     $pidb_id_last = $connection->insert_id;
-    $y_id = $pidb->setYid($pidb_tip_id);    
-    
+    $y_id = $pidb->setYid($pidb_tip_id);
+
     // get articles from proforma and save to dispatch
     $result_pidb_articles = $connection->query("SELECT * FROM pidb_article WHERE pidb_id = '$pidb_id'") or die(mysqli_error($connection));
-    while($row_pidb_article = mysqli_fetch_array($result_pidb_articles)){
+    while($row_pidb_article = $result_pidb_articles->fetch_assoc()){
         $pidb_article_id = $row_pidb_article['id'];
         $article_id = $row_pidb_article['article_id'];
         $article_note = $row_pidb_article['note'];
@@ -40,23 +40,36 @@ if(isset($_GET["exportProformaToDispatch"]) ) {
                         . " VALUES ('$pidb_id_last', '$article_id', '$article_note', '$article_pieces', '$article_price', '$article_discounts', '$article_tax', '$article_weight' )") or die(mysqli_error($connection));
 
         $pidb_article_id_last = $connection->insert_id;
-    
+
         // za svaki artikal u predračunu treba proveriti da li postoji property i ako postoji upisati 
         // ga i za novootvorenu otpremnicu
 
         $result_pidb_articles_propertys = $connection->query("SELECT * FROM pidb_article_property WHERE pidb_article_id = '$pidb_article_id'") or die(mysqli_error($connection));
-        while($row_pidb_articles_property = mysqli_fetch_array($result_pidb_articles_propertys)){
+        while($row_pidb_articles_property = $result_pidb_articles_propertys->fetch_assoc()){
             $property_id = $row_pidb_articles_property['property_id'];
             $quantity = $row_pidb_articles_property['quantity'];
 
             $connection->query("INSERT INTO pidb_article_property (pidb_article_id, property_id, quantity) VALUES ('$pidb_article_id_last', '$property_id', '$quantity' )") or die(mysqli_error($connection));
-  
         }
-    
+
     }
 
     // proforma go to archive
     $connection->query("UPDATE pidb SET archived='1' WHERE id = '$pidb_id' ") or die(mysqli_error($connection));
+
+    // check if proforma has avans payment, if has then save avans to dispatch
+    $result_pidb_avans = $connection->query("SELECT * FROM payment WHERE pidb_id = '$pidb_id'") or die(mysqli_error($connection));
+    if($result_pidb_avans->num_rows) {
+
+        // get avans form proforma $pidb_id
+        $row = $result_pidb_avans->fetch_assoc();
+        $date = $row['date'];
+        $payment_type_id = $row['payment_type_id'];
+        $amount = $row['amount'];
+
+        // save avans to dispatch $pidb_id_last
+        $connection->query("INSERT INTO payment (date, pidb_id, payment_type_id, amount) VALUES ('$date', '$pidb_id_last', '$payment_type_id', '$amount' )") or die(mysqli_error($connection)); 
+    }
 
     die('<script>location.href = "?view&pidb_id='.$pidb_id_last.'&pidb_tip_id='.$pidb_tip_id.'" </script>');
 }
