c8efd6e22baa421fe5d0655945bca398ad7c6901
draganjancikin <dragan.jancikin@gmail.com>
2020-10-29 13:44:45 +0100

    Resolved print problem

--- www/tcpdf/examples/printProjectTaskWithNotes.php
+++ www/tcpdf/examples/printProjectTaskWithNotes.php
@@ -1,5 +1,5 @@
 <?php
-$page = "project";
+$page = "projects";
 require_once('../config/lang/eng.php');
 require_once('../tcpdf.php');
 
@@ -113,7 +113,7 @@ $html = '
   
   <table border="0">
     <tr><td width="80px">klijent:</td> <td width="auto">'.$client_data['name'] . ($client_data['name_note']<>""?', '.$client_data['name_note']:"").'</td></tr>
-    <tr><td>adresa:</td>               <td>'.$client_data['street_name'].' '.$client_data['home_number'].', '.$client_data['city_name'].', '.$client_data['state_name'].', '.$client_data['adress_note'].'</td></tr>
+    <tr><td>adresa:</td>               <td>'.$client_data['street_name'].' '.$client_data['home_number'].', '.$client_data['city_name'].', '.$client_data['state_name'].', '.$client_data['address_note'].'</td></tr>
     <tr><td></td>                      <td>' .$phone1. ', ' .$phone1_note. '</td></tr>
     ' .( $phone2=="" ? "" : '<tr><td></td><td>' .$phone2. ', ' .$phone2_note. '</td></tr>' ). '
   </table>
