f444f812af0a99571ed6d8e5abc6fe186db03efe
draganjancikin <dragan.jancikin@gmail.com>
2020-10-30 13:47:42 +0100

    Resolved issue #10

---
+++ app/classes/DBconnection.class.php
@@ -0,0 +1,30 @@
+<?php
+require_once '/server/app/config/dbConfig.php';
+/**
+ * DBconnection.class.php
+ * 
+ * Database connection class
+ * 
+ * @author Dragan Jancikin <dragan.jancikin@gmail.com>
+ */
+class DBconnection {
+
+    public function __construct() {
+        $this->connection = $this->tryConnect();
+    }
+
+    protected function tryConnect() {
+
+        $conn = new mysqli( DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME );
+        $conn->set_charset("utf8");
+
+        if ( $conn->connect_error ) {
+            printf("Connection failed: %s\ ", $conn->connect_error);
+            exit();
+        }
+
+        // echo "The connection is established!" . "<br>";
+        return $conn;
+    }
+
+}
