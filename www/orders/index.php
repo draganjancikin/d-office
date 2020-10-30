8e182576cadb0c9569fca3d404161a62794645a5
draganjancikin <dragan.jancikin@gmail.com>
2020-10-30 15:50:50 +0100

    Little code update

--- www/orders/index.php
+++ www/orders/index.php
@@ -3,10 +3,10 @@ $page = "orders";
 require_once filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/../app/config/conf.php';
 session_start();
 if(isset($_SESSION['username'])):
-    
+
     $username = $_SESSION['username'];
     $userlevel = $_SESSION['user_level'];
-    
+
     $conf = new Conf();
     $client = new Client();
     $contact = new Contact();
