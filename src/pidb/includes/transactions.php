ebfe7d5a0c59e9cc2d84bb462989733ac38dfb7f
draganjancikin <dragan.jancikin@gmail.com>
2020-11-18 13:17:16 +0100

    delete unnecessary file

--- src/pidb/includes/transactions.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_GET['pidb_id'])) {
-    echo "ima pidb_id, biće listing transakcija za određeni dokument";
-} else {
-    echo "nema pidb_id, biće lista zadnjih xy transakcija";
-}
