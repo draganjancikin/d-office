3e5fc2771ea08db80a367e4595ccf99d9362239c
draganjancikin <dragan.jancikin@gmail.com>
2020-12-15 15:55:41 +0100

    Add table to cashRegister

--- templates/pidb/includes/cashRegister.php
+++ templates/pidb/includes/cashRegister.php
@@ -1,2 +1,30 @@
-<?php
-echo "Biće kasa ...";
+<div class="row">
+    <div class="col-lg-10 col-md-12">
+        <div class="card mb-4">
+            <div class="card-header p-2">
+                <h6 class="m-0 font-weight-bold text-primary">Kasa</h6>
+            </div>
+            <div class="card-body p-2">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover" id="" width="100%" cellspacing="0">
+                        <thead class="thead-light">
+                            <tr>
+                                <th class="text-center">___</th>
+                            </tr>
+                        </thead>
+                        <tfoot class="thead-light">
+                            <tr>
+                                <th class="text-center">___</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+Biće kasa ...
