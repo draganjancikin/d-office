49453153a1ba583f10a6b75419e85724a986a2d8
draganjancikin <dragan.jancikin@gmail.com>
2021-02-12 14:16:36 +0100

    Add method getNumberOfClients()

--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -6,6 +6,20 @@ use Doctrine\ORM\EntityRepository;
 
 class ClientRepository extends EntityRepository {
 
-  
+  /**
+   * Method that return number of clients
+   *
+   * @return int
+   */
+  public function getNumberOfClients() {
+    
+    $qb = $this->_em->createQueryBuilder();
+    $qb->select('count(c.id)');
+    $qb->from('Roloffice\Entity\Client','c');
+
+    $count = $qb->getQuery()->getSingleScalarResult();
+
+    return $count;
+  }
 
 }
