91af5add904d6e83b64c2e1dad4359035e6b7969
draganjancikin <dragan.jancikin@gmail.com>
2021-02-16 15:59:13 +0100

    Rename file listLast.php

--- templates/material/includes/list_last.php
+++ templates/material/includes/list_last.php
@@ -11,7 +11,6 @@
             <th class="text-center">jed. mere</th>
             <th class="text-center">cena <br />(RSD sa PDV-om)</th>
             <th class="text-center">cena <br />(&#8364; sa PDV-om)</th>
-            <th>dobavljač</th>
           </tr>
         </thead>
         <tfoot class="thead-light">
@@ -20,20 +19,21 @@
             <th class="text-center">jed. mere</th>
             <th class="text-center">cena <br />(RSD sa PDV-om)</th>
             <th class="text-center">cena <br />(&#8364; sa PDV-om)</th>
-            <th>dobavljač</th>
           </tr>
         </tfoot>
         <tbody>
         <?php
-        $materials = $material->getLastMaterials(10);
-        foreach ($materials as $material_one):
+        // TODO Dragan
+        // $materials = $material->getLastMaterials(10);
+        $materials = $entityManager->getRepository('\Roloffice\Entity\Material')->getLastMaterials(10);
+        foreach ($materials as $material_data):
+          $material_unit = $entityManager->find('\Roloffice\Entity\Unit', $material_data->getUnit());
           ?>
           <tr>
-            <td><a href="?view&material_id=<?php echo $material_one['id'] ?>"><?php echo $material_one['name'] ?></a></td>
-            <td class="text-center"><?php echo $material_one['unit_name'] ?></td>
-            <td class="text-right"><?php echo number_format( ($material_one['price'] * $material->getKurs() * ($material->getTax()/100 + 1) ) , 2, ",", ".") ?></td>
-            <td class="text-right"><?php echo number_format( ($material_one['price'] * ($material->getTax()/100 + 1) ) , 1, ",", ".") ?></td>
-            <td><?php echo $material_one['client_name'] ?></td>
+            <td><a href="?view&material_id=<?php echo $material_data->getId() ?>"><?php echo $material_data->getName() ?></a></td>
+            <td class="text-center"><?php echo $material_unit->getName() ?></td>
+            <td class="text-right"><?php echo number_format( ($material_data->getPrice() * $material->getKurs() * ($material->getTax()/100 + 1) ) , 2, ",", ".") ?></td>
+            <td class="text-right"><?php echo number_format( ($material_data->getPrice() * ($material->getTax()/100 + 1) ) , 1, ",", ".") ?></td>
           </tr>
           <?php
         endforeach;
