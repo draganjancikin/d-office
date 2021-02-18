0713857ef6ab015c85c721645fc1b068784810dc
draganjancikin <dragan.jancikin@gmail.com>
2021-02-18 09:32:33 +0100

    Links update

--- templates/material/index.php
+++ templates/material/index.php
@@ -43,9 +43,9 @@
                 if (empty($_GET)): // ako je $_GET prazan
                   include '../../templates/material/includes/list_last.php';
                 else:
-                  if(isset($_GET['view'])) include '../../templates/material/includes/formView.php';
-                  if(isset($_GET['edit'])) include '../../templates/material/includes/formEdit.php';
-                  if(isset($_GET['new'])) include '../../templates/material/includes/formNew.php';
+                  if(isset($_GET['viewMaterial'])) include '../../templates/material/includes/formView.php';
+                  if(isset($_GET['editMaterial'])) include '../../templates/material/includes/formEdit.php';
+                  if(isset($_GET['newMaterial'])) include '../../templates/material/includes/form_new_material.php';
                   
                   if(isset($_GET['search'])) include '../../app/includes/search.php';
                 endif;
