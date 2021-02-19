cca5d188f41e10772d7a66253e14e226d40eee8f
draganjancikin <dragan.jancikin@gmail.com>
2021-02-19 09:01:31 +0100

    Create a new Entity: ProjectPriority

---
+++ src/Entity/ProjectPriority.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Roloffice\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="v6_project_priorities")
+ */
+class ProjectPriority {
+
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue
+   * @var int
+   */
+  protected $id;
+
+  /**
+   * @ORM\Column(type="string", unique=TRUE, length=48)
+   * @var string
+   */
+  protected $name;
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function setName($name) {
+    $this->name = $name;
+  }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+}
