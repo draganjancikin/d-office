b40974c6aabaa420bccdf00339b803809d992dfc
draganjancikin <dragan.jancikin@gmail.com>
2021-03-01 10:43:43 +0100

    File renamed

--- templates/pidb/includes/form_preferences.php
+++ templates/pidb/includes/form_preferences.php
@@ -1,25 +1,26 @@
 <!-- Settings Data -->
 <?php
 if (isset($_GET['set'])):
+  $preferences = $entityManager->find('Roloffice\Entity\Preferences', 1);
   ?>
   <div class="card mb-4">
     <div class="card-header p-2">
       <h6 class="m-0 text-dark">Podešavanja</h6>
     </div>
     <div class="card-body p-2">
-      <form action="<?php echo $_SERVER['PHP_SELF'] . '?editSettings'; ?>" method="post">
+      <form action="<?php echo $_SERVER['PHP_SELF'] . '?updatePreferences'; ?>" method="post">
         
         <div class="form-group row">
           <label for="inputKurs" class="col-sm-3 col-lg-2 col-form-label text-right">Kurs EUR: </label>
           <div class="col-sm-5">
-            <input id="inputKurs" class="form-control" type="text" name="kurs" value="<?php echo $pidb->getKurs(); ?>" required />
+            <input id="inputKurs" class="form-control" type="text" name="kurs" value="<?php echo $preferences->getKurs(); ?>" required />
           </div>
         </div>
 
         <div class="form-group row">
           <label for="inputTax" class="col-sm-3 col-lg-2 col-form-label text-right">PDV: </label>
           <div class="col-sm-5">
-            <input id="inputTax" class="form-control" type="text" name="tax" value="<?php echo $pidb->getTax(); ?>" required />
+            <input id="inputTax" class="form-control" type="text" name="tax" value="<?php echo $preferences->getTax(); ?>" required />
           </div>
         </div>
 
