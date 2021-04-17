cbb4f5ec88df191623f9629bc1f5fb22a2fdc524
Dragan Jancikin <dragan.jancikingmail.com>
2021-04-17 09:50:49 +0200

    dbConfig files update

--- config/appConfig.php
+++ config/appConfig.php
@@ -1,21 +1,6 @@
 <?php
 require_once 'dbConfig.php';
 
-/* TODO: 
-- dbConfig on LIVE change to dbConfig.live.php
-- dbConfig on DEV change to dbConfig.dev.php
-- dbConfig on LOCAL change to dbConfig.local.php
-
-# Check enviroment and include dbConfig file
-if (file_exists(__DIR__ . '/dbConfig.live.php')) {
-  include '/dbConfig.live.php';
-} else if (file_exists(__DIR__ . '/dbConfig.dev.php')) {
-  include '/dbConfig.dev.php';
-} else if (file_exists(__DIR__ . '/dbConfig.local.php')) {
-  include '/dbConfig.local.php';
-} 
-*/
-
 define("VERSION","5.3.5-6.0.5");
 
 if(empty($page)) {
---
+++ config/dbConfig.php
@@ -0,0 +1,11 @@
+<?php
+# Check enviroment and include dbConfig file.
+if (file_exists(__DIR__ . '/dbConfig.live.php')) {
+  include __DIR__ . '/dbConfig.live.php';
+} else if (file_exists(__DIR__ . '/dbConfig.dev.php')) {
+  include __DIR__ . '/dbConfig.dev.php';
+} else if (file_exists(__DIR__ . '/dbConfig.local.php')) {
+  include __DIR__ . '/dbConfig.local.php';
+} else {
+  echo "Enviroments files don't present!";
+}
