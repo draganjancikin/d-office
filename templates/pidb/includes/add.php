222cb6fd5f2600110b36570981feff9b8724f548
Dragan Jancikin <dragan.jancikin@gmail.com>
2021-07-04 10:00:22 +0200

    Code cleanup

--- templates/pidb/includes/add.php
+++ templates/pidb/includes/add.php
@@ -1,42 +1,4 @@
 <?php
-// use Roloffice\Core\Database;
-
-// add new document
-/*
-if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addPidb"]) ) {
-
-  $user_id = $_SESSION['user_id'];
-  $date = date('Y-m-d h:i:s');
-  $client_id = htmlspecialchars($_POST["client_id"]);
-  $pidb_tip_id = htmlspecialchars($_POST["pidb_tip_id"]);
-
-  if(isset($_POST["project_id"])) {
-    $project_id = htmlspecialchars($_POST["project_id"]);
-  } else {
-    $project_id = 0;
-  }
-
-  $title = htmlspecialchars($_POST["title"]);
-  $note = htmlspecialchars($_POST["note"]);
-
-  $db = new Database();
-    
-  $db->connection->query("INSERT INTO pidb (tip_id, date, client_id, project_id, title, note) " 
-                        . " VALUES ('$pidb_tip_id', '$date', '$client_id', '$project_id', '$title', '$note')") or die(mysqli_error($connect_db));
-  
-  $pidb_id = $db->connection->insert_id;
-
-  // if exist project_id write project_id and pidb_id to table project_pidb
-  if(isset($_POST["project_id"])){
-    $project_id = htmlspecialchars($_POST["project_id"]);
-    $db->connection->query("INSERT INTO project_pidb (project_id, pidb_id) VALUES ('$project_id', '$pidb_id')") or die(mysqli_error($db->connection)); 
-  }
-
-  $y_id = $pidb->setYid($pidb_tip_id);
-
-  die('<script>location.href = "?view&pidb_id='.$pidb_id.'&pidb_tip_id='.$pidb_tip_id.'" </script>');
-}
-*/
 // add article in document
 if($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["addArticleInPidb"]) ) {
 
