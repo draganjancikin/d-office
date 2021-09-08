e1dad9dfee0da5ddd27f3454a10d0f47640a7b1d
Dragan Jancikin <dragan.jancikin@gmail.com>
2021-09-08 10:06:05 +0200

    Resolved issue #116: Add same material in order error

--- templates/order/includes/duplicate_material_in_order.php
+++ templates/order/includes/duplicate_material_in_order.php
@@ -1,26 +1,43 @@
 <?php
 
-// Duplicate Material in Order
+// Duplicate Material in Order.
 if($_SERVER["REQUEST_METHOD"] == "GET" AND isset($_GET["duplicateMaterialInOrder"])) {
-    
-    $order_id = htmlspecialchars($_GET["order_id"]);
-    
-    $order_material_id = htmlspecialchars($_GET["order_material_id"]);
-    $orderMaterial = $entityManager->find("\Roloffice\Entity\OrderMaterial", $order_material_id);
 
-    $newOrderMaterial = new \Roloffice\Entity\OrderMaterial();
+  $order_id = htmlspecialchars($_GET["order_id"]);
 
-    $newOrderMaterial->setOrder($orderMaterial->getOrder());
-    $newOrderMaterial->setMaterial($orderMaterial->getMaterial());
-    $newOrderMaterial->setPieces($orderMaterial->getPieces());
-    $newOrderMaterial->setPrice($orderMaterial->getPrice());
-    $newOrderMaterial->setDiscount(0);
-    $newOrderMaterial->setTax($orderMaterial->getTax());
-    $newOrderMaterial->setWeight($orderMaterial->getWeight());
-    $newOrderMaterial->setNote($orderMaterial->getNote());
+  $order_material_id = htmlspecialchars($_GET["order_material_id"]);
+  $orderMaterial = $entityManager->find("\Roloffice\Entity\OrderMaterial", $order_material_id);
 
-    $entityManager->persist($newOrderMaterial);
+  $newOrderMaterial = new \Roloffice\Entity\OrderMaterial();
+
+  $newOrderMaterial->setOrder($orderMaterial->getOrder());
+  $newOrderMaterial->setMaterial($orderMaterial->getMaterial());
+  $newOrderMaterial->setPieces($orderMaterial->getPieces());
+  $newOrderMaterial->setPrice($orderMaterial->getPrice());
+  $newOrderMaterial->setDiscount(0);
+  $newOrderMaterial->setTax($orderMaterial->getTax());
+  $newOrderMaterial->setWeight($orderMaterial->getWeight());
+  $newOrderMaterial->setNote($orderMaterial->getNote());
+
+  $entityManager->persist($newOrderMaterial);
+  $entityManager->flush();
+
+  // Get Properties from old OrderMaterial and add to newOrderMaterial
+  $material_on_order_properties = $entityManager->getRepository('\Roloffice\Entity\OrderMaterial')->getProperties($order_material_id);
+
+  foreach ($material_on_order_properties as $material_on_order_property) {
+
+    // insert to table v6__orders__materials__properties
+    $newOrderMaterialProperty = new \Roloffice\Entity\OrderMaterialProperty();
+
+    $newOrderMaterialProperty->setOrderMaterial($newOrderMaterial);
+    $newOrderMaterialProperty->setProperty($material_on_order_property->getProperty());
+    $newOrderMaterialProperty->setQuantity(0);
+
+    $entityManager->persist($newOrderMaterialProperty);
     $entityManager->flush();
 
-    die('<script>location.href = "?edit&order_id='.$order_id.'" </script>');
+  }
+
+  die('<script>location.href = "?edit&order_id='.$order_id.'" </script>');
 }
