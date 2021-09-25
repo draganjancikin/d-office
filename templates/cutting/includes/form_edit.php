ca3e94a8724620be078ea37f90764bb7a89ec8ab
Dragan Jancikin <dragan.jancikin@gmail.com>
2021-09-25 16:44:27 +0200

    Resolved issue #122: Cutting Sheets calculations is no good

--- templates/cutting/includes/form_edit.php
+++ templates/cutting/includes/form_edit.php
@@ -92,9 +92,9 @@
               </tr>
             </form>
             <?php
-            $cutting_sheet__article__picket_number = $entityManager->getRepository('\Roloffice\Entity\CuttingSheetArticle')->getCuttingSheetArticlePicketNumber($cutting_sheet_article->getId()) * $cutting_sheet_article->getNumberOfFields();
+            $cutting_sheet__article__picket_number = $entityManager->getRepository('\Roloffice\Entity\CuttingSheetArticle')->getPicketsNumber($cutting_sheet_article->getId()) * $cutting_sheet_article->getNumberOfFields();
 
-            $cutting_sheet__article__picket_lenght = $entityManager->getRepository('\Roloffice\Entity\CuttingSheetArticle')->getArticlePicketLength($cutting_sheet_article->getId()) * $cutting_sheet_article->getNumberOfFields();
+            $cutting_sheet__article__picket_lenght = $entityManager->getRepository('\Roloffice\Entity\CuttingSheetArticle')->getPicketsLength($cutting_sheet_article->getId()) * $cutting_sheet_article->getNumberOfFields();
 
             $total_kap = $total_kap + $cutting_sheet__article__picket_number;
             $total_picket_lenght = $total_picket_lenght + $cutting_sheet__article__picket_lenght;
