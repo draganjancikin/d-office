2b5cf055a0d8d4b829c30d01126dbd5a9eb81f19
Dragan Jancikin <dragan.jancikin@gmail.com>
2021-12-23 11:32:19 +0100

    Delete unnecessary old web/autoload.php

--- web/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-function myAutoLoader($class) {
-    $path = $_SERVER['DOCUMENT_ROOT'] . '/../src/Controller/' . $class . ".php";
-    include $path;
-}
-
-spl_autoload_register('myAutoLoader');
