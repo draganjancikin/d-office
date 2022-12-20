f8f06a301c6ce5391acb339981788fba01f492be
Dragan Jancikin <dragan.jancikin@gmail.com>
2022-12-20 09:08:43 +0100

    Code refactoring

--- src/Entity/Preferences.php
+++ src/Entity/Preferences.php
@@ -10,44 +10,58 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Preferences {
 
-  /**
-   * @ORM\Id
-   * @ORM\Column(type="integer")
-   * @ORM\GeneratedValue
-   * @var int
-   */
-  protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var int
+     */
+    protected $id;
 
-  /**
-   * @ORM\Column(type="decimal", precision=11, scale=4)
-   * @var float
-   */
-  protected $kurs;
+    /**
+     * @ORM\Column(type="decimal", precision=11, scale=4)
+     * @var float
+     */
+    protected $kurs;
 
-  /**
-   * @ORM\Column(type="decimal", precision=11, scale=4)
-   * @var float
-   */
-  protected $tax;
+    /**
+     * @ORM\Column(type="decimal", precision=11, scale=4)
+     * @var float
+     */
+    protected $tax;
 
-  public function getId() {
-    return $this->id;
-  }
+    /**
+     * @ORM\Column(type="string", length=128)
+     * @var string
+     */
+    protected $local_backup_folder;
 
-  public function setKurs($kurs) {
-    $this->kurs = $kurs;
-  }
+    public function getId() {
+        return $this->id;
+    }
 
-  public function getKurs() {
-    return $this->kurs;
-  }
+    public function setKurs($kurs) {
+        $this->kurs = $kurs;
+    }
 
-  public function setTax($tax) {
-    $this->tax = $tax;
-  }
+    public function getKurs() {
+        return $this->kurs;
+    }
 
-  public function getTax() {
-    return $this->tax;
-  }
+    public function setTax($tax) {
+        $this->tax = $tax;
+    }
+
+    public function getTax() {
+        return $this->tax;
+    }
+
+    public function setLocalBackupFolder($local_backup_folder) {
+        $this->local_backup_folder = $local_backup_folder;
+    }
+
+    public function getLocalBackupFolder() {
+        return $this->local_backup_folder;
+    }
 
 }
