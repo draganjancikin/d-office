5cb8ee1b3069e547da35bad26b67d371ae3c888a
Nenad Opancarev <nopancarev@gmail.com>
2022-12-23 13:51:33 +0100

    new-document-edit

--- templates/pidb/includes/form_new.php
+++ templates/pidb/includes/form_new.php
@@ -4,12 +4,13 @@
     <h6 class="m-0 text-dark">Otvaranje novog dokumenta:</h6>
   </div>
   <div class="card-body p-2">
-    <form class="form-horizontal" role="form" action="<?php echo $_SERVER['PHP_SELF'] . '?createAccountingDocument'; ?>" method="post">
+    <form class="form-horizontal" role="form" action="<?php echo $_SERVER['PHP_SELF'] . '?createAccountingDocument'; ?>"
+      method="post">
       <?php if (isset($_GET['project_id'])): ?>
-        <input type="hidden" name="project_id" value="<?php echo $_GET['project_id'] ?>">
+      <input type="hidden" name="project_id" value="<?php echo $_GET['project_id'] ?>">
       <?php endif; ?>
 
-      <div class="form-group row">
+      <div class="form-group row text-nowrap">
         <label for="select_pidb_type_id" class="col-sm-3 col-lg-2 col-form-label text-right">Vrsta dokumenta:</label>
         <div class="col-sm-4">
           <div id="type">
@@ -26,7 +27,7 @@
         <label for="select_client_id" class="col-sm-3 col-lg-2 col-form-label text-right">Klijent: </label>
         <div class="col-sm-4">
           <select id="select_client_id" class="form-control" name="client_id" required="required">
-          <?php
+            <?php
           if (isset($_GET['client_id'])){
             $client_id = htmlspecialchars($_GET["client_id"]);
             $client_data = $entityManager->find('\Roloffice\Entity\Client', $client_id);
@@ -45,10 +46,10 @@
           ?>
           </select>
         </div>
-      </div>  
-    
+      </div>
+
       <div class="form-group row">
-        <label for="input_title" class="col-sm-3 col-lg-2 col-form-label text-right" >Naslov: </label>
+        <label for="input_title" class="col-sm-3 col-lg-2 col-form-label text-right">Naslov: </label>
         <div class="col-sm-6">
           <input id="input_title" class="form-control" type="text" name="title" value="" />
         </div>
@@ -56,18 +57,19 @@
 
       <div class="form-group row">
         <label for="input_note" class="col-sm-3 col-lg-2 col-form-label text-right">Beleška: </label>
-        <div class="col-md-10">
-          <textarea id="input_note" class="form-control" rows="3" name="note" placeholder="Unesite belešku uz dokument"></textarea>
+        <div class="col-md-6">
+          <textarea id="input_note" class="form-control" rows="3" name="note"
+            placeholder="Unesite belešku uz dokument"></textarea>
         </div>
-        </div>
+      </div>
 
-        <div class="form-group row">
+      <div class="form-group row">
         <div class="col-sm-3 offset-sm-3 offset-lg-2">
           <button type="submit" class="btn btn-sm btn-success" title="Snimi izmene podataka o klijentu!">
             <i class="fas fa-save"></i> Snimi
           </button>
         </div>
-        </div>
+      </div>
 
     </form>
   </div>
