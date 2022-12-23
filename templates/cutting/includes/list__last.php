a4f9bfbb4a698ec55d0ae42a5c590e9d722ebcf6
Nenad Opancarev <nopancarev@gmail.com>
2022-12-23 15:31:52 +0100

    cutting-edit-text

--- templates/cutting/includes/list__last.php
+++ templates/cutting/includes/list__last.php
@@ -3,7 +3,7 @@
     <h6 class="m-0 font-weight-bold text-primary">Zadnje krojne liste</h6>
   </div>
   <div class="card-body p-2">
-    <div class="table-responsive">
+    <div class="table-responsive text-nowrap">
       <table class="table table-bordered table-hover" id="" width="100%" cellspacing="0">
         <thead class="thead-light">
           <tr>
@@ -22,15 +22,16 @@
           $cutting_sheets = $entityManager->getRepository('\Roloffice\Entity\CuttingSheet')->getLastCuttingSheets(10);
           foreach ($cutting_sheets as $cutting_sheet):
             ?>
-            <tr>
-              <td class="centar">
-                <a href="?view&id=<?php echo $cutting_sheet->getId() ?>">KL_<?php echo str_pad($cutting_sheet->getOrdinalNumInYear(), 4, "0", STR_PAD_LEFT) ?></a>
-              </td>
-              <td>
-                <?php echo $cutting_sheet->getClient()->getName() ?>
-              </td>
-            </tr>
-            <?php
+          <tr>
+            <td class="centar">
+              <a
+                href="?view&id=<?php echo $cutting_sheet->getId() ?>">KL_<?php echo str_pad($cutting_sheet->getOrdinalNumInYear(), 4, "0", STR_PAD_LEFT) ?></a>
+            </td>
+            <td>
+              <?php echo $cutting_sheet->getClient()->getName() ?>
+            </td>
+          </tr>
+          <?php
           endforeach;
           ?>
         </tbody>
