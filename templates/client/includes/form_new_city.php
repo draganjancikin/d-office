a43e84e283dd5c7011c6e1d6d9f8971c553ac32e
Dragan Jancikin <dragan.jancikin@gmail.com>
2022-12-24 09:51:31 +0100

    Implementing Nenads changes on: form_street_edit, form_city_edit and form_new_country

--- templates/client/includes/form_new_city.php
+++ templates/client/includes/form_new_city.php
@@ -1,22 +1,22 @@
-<!-- New City Data -->
+<!-- Form: new City -->
 <div class="card mb-4">
-  <div class="card-header p-2">
-    <h6 class="m-0 font-weight-bold text-primary">Unos novog naselja </h6>
-  </div>
-  <div class="card-body">
-    <form action="<?php echo $_SERVER['PHP_SELF'] . '?createCity'; ?>" method="post">
-      <input type="hidden" name="action" value="city">
-      <div class="form-group row">
-        <label for="inputName" class="col-sm-4 col-md-5 col-lg-4 col-form-label text-left text-sm-right">Unesi naziv naselja: </label>
-        <div class="col-sm-8 col-md-7 col-lg-8">
-          <input id="inputName" class="form-control" type="text" name="name" required />
-        </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-sm-3 offset-sm-4 offset-md-5 offset-lg-4">
-          <button type="submit" class="btn btn-sm btn-success">Snimi</button>
-        </div>
-      </div>
-    </form>
-  </div>
+    <div class="card-header p-2">
+        <h6 class="m-0 font-weight-bold text-primary">Unos novog naselja </h6>
+    </div>
+    <div class="card-body">
+        <form action="<?php echo $_SERVER['PHP_SELF'] . '?createCity'; ?>" method="post">
+            <input type="hidden" name="action" value="city">
+            <div class="form-group row">
+                <label for="inputName" class="col-sm-3 col-md-4 col-lg-3 col-xl-2 col-form-label text-left text-sm-right">Naselje: </label>
+                <div class="col-sm-9 col-md-8 col-lg-9 col-xl-10">
+                    <input id="inputName" class="form-control" type="text" name="name" placeholder="Unesite naziv naselja" required />
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-3 offset-sm-3 offset-md-4 offset-lg-3 offset-xl-2">
+                    <button type="submit" class="btn btn-sm btn-success">Snimi</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
