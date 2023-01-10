16d2c72c8c5379dc44c533f461700fc89c57d16d
Dragan Jancikin <dragan.jancikin@gmail.com>
2023-01-10 08:52:44 +0100

    In progress #182: Create a section for create a new type of project task - part 1

---
+++ templates/project/includes/project_tasks.php
@@ -0,0 +1,9 @@
+U izradi ...
+<hr>
+Sadrzaj stranice:
+<ul>
+    <li>Predefinisani projektni zadaci</li>
+    <li>Listing ...</li>
+    <li>Dodavanje novih kroz modal ...</li>
+    <li>Izmena postojecih kroz modal ...</li>
+</ul>
