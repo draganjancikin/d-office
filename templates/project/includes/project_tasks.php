7aeb7c61d7656fcb9186caf7154687e8c52fd101
Dragan Jancikin <dragan.jancikin@gmail.com>
2023-01-12 23:39:04 +0100

    Task update

--- templates/project/includes/project_tasks.php
+++ templates/project/includes/project_tasks.php
@@ -6,4 +6,5 @@ Sadrzaj stranice:
     <li>Listing ...</li>
     <li>Dodavanje novih kroz modal ...</li>
     <li>Izmena postojecih kroz modal ...</li>
+    <li>Brisanje (u stvari nece se brisati vec samo oznaciti kao brisan, da bi se izbegli problemi sa nepostojecim id-jem)</li>
 </ul>
