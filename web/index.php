24cb22f8bcf93e60670f6ee7d860f1de28ab08e3
Dragan Jancikin <dragan.jancikin@gmail.com>
2023-01-28 22:38:49 +0100

    Refactor application Front Controller (index.php)

--- web/index.php
+++ web/index.php
@@ -48,11 +48,10 @@ if (isset($map[$path])) {
         ob_start();
         extract($request->query->all(), EXTR_SKIP);
         include sprintf(__DIR__.'/../templates/%s.php', $map[$path]);
-        $response = new Response(ob_get_clean());
     } else {
         include '../templates/formLogin.php';
-        $response = new Response(ob_get_clean());
     }
+    $response = new Response(ob_get_clean());
 
 } else {
     $response = new Response('Not Found', 404);
