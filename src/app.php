be1c4bc917e3e42aef7bd0d901adc279a0b19326
Dragan Jancikin <dragan.jancikin@gmail.com>
2023-01-30 00:35:02 +0100

    Refactoring front controller

--- src/app.php
+++ src/app.php
@@ -4,34 +4,77 @@ use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 
-$routes->add('index', new Routing\Route('/'));
+$routes->add('index', new Routing\Route('/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('client/index', new Routing\Route('/clients/'));
+$routes->add('client/index', new Routing\Route('/clients/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('pidb/index', new Routing\Route('/pidb/'));
-$routes->add('pidb/printAccountingDocument', new Routing\Route('/pidb/printAccountingDocument'));
-$routes->add('pidb/printAccountingDocumentW', new Routing\Route('/pidb/printAccountingDocumentW'));
-$routes->add('pidb/printAccountingDocumentI', new Routing\Route('/pidb/printAccountingDocumentI'));
-$routes->add('pidb/printAccountingDocumentIW', new Routing\Route('/pidb/printAccountingDocumentIW'));
-$routes->add('pidb/printDailyCashReport', new Routing\Route('/pidb/printDailyCashReport'));
+$routes->add('pidb/index', new Routing\Route('/pidb/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('pidb/printAccountingDocument', new Routing\Route('/pidb/printAccountingDocument', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('pidb/printAccountingDocumentW', new Routing\Route('/pidb/printAccountingDocumentW', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('pidb/printAccountingDocumentI', new Routing\Route('/pidb/printAccountingDocumentI', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('pidb/printAccountingDocumentIW', new Routing\Route('/pidb/printAccountingDocumentIW', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('pidb/printDailyCashReport', new Routing\Route('/pidb/printDailyCashReport', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('cutting/index', new Routing\Route('/cutting/'));
-$routes->add('cutting/printCutting', new Routing\Route('/cutting/printCutting'));
+$routes->add('cutting/index', new Routing\Route('/cutting/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('cutting/printCutting', new Routing\Route('/cutting/printCutting', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('material/index', new Routing\Route('/materials/'));
+$routes->add('material/index', new Routing\Route('/materials/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('order/index', new Routing\Route('/orders/'));
-$routes->add('order/printOrder', new Routing\Route('/orders/printOrder'));
+$routes->add('order/index', new Routing\Route('/orders/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('order/printOrder', new Routing\Route('/orders/printOrder', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('article/index', new Routing\Route('/articles/'));
+$routes->add('article/index', new Routing\Route('/articles/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('project/index', new Routing\Route('/projects/'));
-$routes->add('project/printProjectTask', new Routing\Route('/projects/printProjectTask'));
-$routes->add('project/printInstallationRecord', new Routing\Route('/projects/printInstallationRecord'));
-$routes->add('project/printProjectTaskWithNotes', new Routing\Route('/projects/printProjectTaskWithNotes'));
+$routes->add('project/index', new Routing\Route('/projects/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('project/printProjectTask', new Routing\Route('/projects/printProjectTask', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('project/printInstallationRecord', new Routing\Route('/projects/printInstallationRecord', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
+$routes->add('project/printProjectTaskWithNotes', new Routing\Route('/projects/printProjectTaskWithNotes', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
-$routes->add('admin/index', new Routing\Route('/admin/'));
+$routes->add('admin/index', new Routing\Route('/admin/', [
+    '_controller' => function ($request) { return render_template($request); }
+]));
 
 //$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
+
+//$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+//  'year' => null,
+//  '_controller' => [new LeapYearController(), 'index'],
+//]));
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -6,6 +6,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request) {
+    require_once __DIR__.'/../config/bootstrap.php';
+    session_start();
+    if (isset($_SESSION['username'])){
+        $username = $_SESSION['username'];
+        $user_role_id = $_SESSION['user_role_id'];
+        extract($request->attributes->all(), EXTR_SKIP);
+        ob_start();
+        include sprintf(__DIR__.'/../templates/%s.php', $_route);
+    } else {
+      include '../templates/formLogin.php';
+    }
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
@@ -14,20 +29,8 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-
-    require_once __DIR__ . '/../config/bootstrap.php';
-    session_start();
-    if (isset($_SESSION['username'])){
-        $username = $_SESSION['username'];
-        $user_role_id = $_SESSION['user_role_id'];
-        extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-        ob_start();
-        include sprintf(__DIR__.'/../templates/%s.php', $_route);
-    } else {
-        include '../templates/formLogin.php';
-    }
-    $response = new Response(ob_get_clean());
-
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
@@ -36,4 +39,4 @@ try {
 
 $response->send();
 
-// https://symfony.com/doc/5.4/create_framework/templating.html
+// https://symfony.com/doc/5.4/create_framework/http_kernel_controller_resolver.html
