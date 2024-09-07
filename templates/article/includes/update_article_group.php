3f4ad25cec22cab716ecd565d52a8c06cc69b3e1
Dragan Jancikin <dragan.jancikin@gmail.com>
2024-09-07 12:31:27 +0200

    #162 - Create a form for create and edit Article group

---
+++ templates/article/includes/update_article_group.php
@@ -0,0 +1,17 @@
+<?php
+// Update Article Group.
+if ($_SERVER["REQUEST_METHOD"] == "POST" AND isset($_GET["updateArticleGroup"]) ) {
+	$name = htmlspecialchars($_POST["name"]);
+	$article_group_id = htmlspecialchars($_GET["article_group_id"]);
+  $article_group = $entityManager->find('\Roloffice\Entity\ArticleGroup', $article_group_id);
+
+  if ($article_group === null) {
+    echo "Article Group with ID $article_group_id does not exist.\n";
+    exit(1);
+  }
+
+  $article_group->setName($name);
+  $entityManager->flush();
+
+  die('<script>location.href = "?viewArticleGroup&article_group_id='.$article_group_id.'" </script>');
+}
