feb9ed36c6fd615b36114a88d7b65f05b1e369bd
Dragan Jancikin <dragan.jancikin@gmail.com>
2024-12-28 10:05:37 +0100

    #259 - Move isUserNotLoggedIn() to BaseController

--- src/Core/BaseController.php
+++ src/Core/BaseController.php
@@ -18,10 +18,21 @@ class BaseController {
    * BaseController constructor.
    */
   public function __construct() {
-    $this->user_id = $_SESSION['user_id'];
+    if (isset($_SESSION['user_id'])) $this->user_id = $_SESSION['user_id'];
     if (isset($_SESSION['username'])) $this->username = $_SESSION['username'];
     if (isset($_SESSION['user_role_id'])) $this->user_role_id = $_SESSION['user_role_id'];
     $this->entityManager = EntityManagerFactory::getEntityManager();
+  }
+
+  /**
+   * Redirect the user to the login page if they are not logged in.
+   *
+   * @return void
+   */
+  public function isUserNotLoggedIn() {
+    if ($this->username === NULL)  {
+      header("Location: /login");
+    }
   }
 
 }
