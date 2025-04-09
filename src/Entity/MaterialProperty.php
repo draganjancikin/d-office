eadfdf75426b1f714c7754c4f5a3b79efb0ab9f4
Dragan Jancikin <dragan.jancikin@gmail.com>
2025-04-09 07:57:33 +0200

    #284 - Replace annotation with attributes on entities: MaterialController, Material, MaterialProperty, MaterialSupplier, MaterialPropertyRepository,       MaterialRepository and MaterialSupplierRepository

--- src/Entity/MaterialProperty.php
+++ src/Entity/MaterialProperty.php
@@ -2,84 +2,97 @@
 
 namespace App\Entity;
 
+use App\Entity\Material;
+use App\Entity\Property;
+use App\Repository\MaterialPropertyRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity (repositoryClass="App\Repository\MaterialPropertyRepository")
- * @ORM\Table(name="v6__materials__properties")
+ * MaterialProperty Entity.
  */
-class MaterialProperty {
+#[ORM\Entity(repositoryClass: MaterialPropertyRepository::class)]
+#[ORM\Table(name: 'v6__materials__properties')]
+class MaterialProperty
+{
 
-  /**
-   * @ORM\Id
-   * @ORM\Column(type="integer")
-   * @ORM\GeneratedValue
-   * @var int
-   */
-  protected $id;
+    /**
+     * Identifier of the MaterialProperty.
+     *
+     * @var int
+     */
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    protected $id;
 
-  /**
-   * Meny ...
-   * @ORM\ManyToOne(targetEntity="Material")
-   * @ORM\JoinColumn(name="material_id", referencedColumnName="id")
-   * @var int
-   */
-  protected $material;
+    /**
+     * Many Materials have the One Property.
+     *
+     * @var int
+     */
+    #[ORM\ManyToOne(targetEntity: Material::class)]
+    #[ORM\JoinColumn(name: 'material_id', referencedColumnName: 'id')]
+    protected $material;
 
-  /**
-   * Meny ...
-   * @ORM\ManyToOne(targetEntity="Property")
-   * @ORM\JoinColumn(name="property_id", referencedColumnName="id")
-   * @var int
-   */
-  protected $property;
+    /**
+     * Many Properties have the One Material.
+     *
+     * @var int
+     */
+    #[ORM\ManyToOne(targetEntity: Property::class)]
+    #[ORM\JoinColumn(name: 'property_id', referencedColumnName: 'id')]
+    protected $property;
 
-  /**
-   * @ORM\Column(type="decimal", precision=11, scale=0)
-   * @var float
-   */
-  protected $min_size;
+    /**
+     * Material Property minimum size.
+     *
+     * @var float
+     */
+    #[ORM\Column(type: 'decimal', precision: 11, scale: 0)]
+    protected $min_size;
 
-  /**
-   * @ORM\Column(type="decimal", precision=11, scale=0)
-   * @var float
-   */
-  protected $max_size;
+    /**
+     * Material Property maximum size.
+     *
+     * @var float
+     */
+    #[ORM\Column(type: 'decimal', precision: 11, scale: 0)]
+    protected $max_size;
 
-  public function getId() {
-    return $this->id;
-  }
+    public function getId() {
+        return $this->id;
+    }
 
-  public function setMaterial($material) {
-    $this->material = $material;
-  }
+    public function setMaterial($material) {
+        $this->material = $material;
+    }
 
-  public function getMaterial() {
-    return $this->material;
-  }
+    public function getMaterial() {
+        return $this->material;
+    }
 
-  public function setProperty($property) {
-    $this->property = $property;
-  }
+    public function setProperty($property) {
+        $this->property = $property;
+    }
 
-  public function getProperty() {
-    return $this->property;
-  }
+    public function getProperty() {
+        return $this->property;
+    }
 
-  public function setMinSize($min_size) {
-    $this->min_size = $min_size;
-  }
+    public function setMinSize($min_size) {
+        $this->min_size = $min_size;
+    }
 
-  public function getMinSize() {
-    return $this->min_size;
-  }
+    public function getMinSize() {
+        return $this->min_size;
+    }
 
-  public function setMaxSize($max_size) {
-    $this->max_size = $max_size;
-  }
+    public function setMaxSize($max_size) {
+        $this->max_size = $max_size;
+    }
 
-  public function getMaxSize() {
-    return $this->max_size;
-  }
+    public function getMaxSize() {
+        return $this->max_size;
+    }
 
 }
