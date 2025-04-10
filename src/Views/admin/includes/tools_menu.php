8b2a3d31237dbcd02b96e3ff6fc106653cfe6ef6
Dragan Jancikin <dragan.jancikin@gmail.com>
2025-04-10 22:56:37 +0200

    #284 - Refactor and improvement of the code

--- src/Views/admin/includes/tools_menu.php
+++ src/Views/admin/includes/tools_menu.php
@@ -1,37 +1,36 @@
 <div class="col-lg-12 col-xl-10 px-2" id="topMeni">
-  <div class="card mb-2">
-    <div class="card-body p-1">
-      <?php
-      if (isset($_GET['url'])) {
-      $url = $_GET['url'];
-      $url = explode('/', $url);
+    <div class="card mb-2">
+        <div class="card-body p-1">
+            <?php
+            if (isset($_GET['url'])) {
+            $url = $_GET['url'];
+            $url = explode('/', $url);
 
-        // In view case show edit button.
-        if (count($url) == 2 && $url[1] == 'company-info') {
-          ?>
-          <a href="/admin/company-info/edit">
-            <button type="button" class="btn btn-sm btn-outline-secondary mx-1" title="Idi na stranicu za izmenu podataka o klijentu!">
-              <i class="fas fa-edit"> </i> Izmena
-            </button>
-          </a>
-          <?php
-        }
+                // In view case show edit button.
+                if (count($url) == 2 && $url[1] == 'company-info') {
+                    ?>
+                    <a href="/admin/company-info/edit">
+                        <button type="button" class="btn btn-sm btn-outline-secondary mx-1" title="Idi na stranicu za izmenu podataka o klijentu!">
+                            <i class="fas fa-edit"> </i> Izmena
+                        </button>
+                    </a>
+                    <?php
+                }
 
-        // In edit case show view button.
-        if (count($url) == 3 && $url[1] == 'company-info' && $url[2] == 'edit') {
-
-          ?>
-          <a href="/admin/company-info">
-            <button type="button" class="btn btn-sm btn-outline-secondary mx-1" title="Idi na stranicu za pregled podataka o klijentu">
-              <i class="fas fa-eye"> </i> Pregled
-            </button>
-          </a>
-          <?php
-        }
-      }
-      ?>
+                // In edit case show view button.
+                if (count($url) == 3 && $url[1] == 'company-info' && $url[2] == 'edit') {
+                    ?>
+                    <a href="/admin/company-info">
+                        <button type="button" class="btn btn-sm btn-outline-secondary mx-1" title="Idi na stranicu za pregled podataka o klijentu">
+                            <i class="fas fa-eye"> </i> Pregled
+                        </button>
+                    </a>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+        <!-- End of Card Body. -->
     </div>
-    <!-- End of Card Body. -->
-  </div>
-  <!-- End of Card. -->
+    <!-- End of Card. -->
 </div>
