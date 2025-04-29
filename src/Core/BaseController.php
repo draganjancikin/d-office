6b02780c31a7c9d14078e2ec52d63754d43c2a4b
Dragan Jancikin <dragan.jancikin@gmail.com>
2025-04-29 15:05:29 +0200

    #297 - Setup twig in BaseConteoller

--- src/Core/BaseController.php
+++ src/Core/BaseController.php
@@ -2,6 +2,9 @@
 
 namespace App\Core;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 /**
  * BaseController class.
  *
@@ -9,30 +12,50 @@ namespace App\Core;
  */
 class BaseController {
 
-  public $user_id;
-  public $username;
-  public $user_role_id;
-  public $entityManager;
+    public $user_id;
+    public $username;
+    public $user_role_id;
+    public $entityManager;
+    protected $twig;
 
-  /**
-   * BaseController constructor.
-   */
-  public function __construct() {
-    if (isset($_SESSION['user_id'])) $this->user_id = $_SESSION['user_id'];
-    if (isset($_SESSION['username'])) $this->username = $_SESSION['username'];
-    if (isset($_SESSION['user_role_id'])) $this->user_role_id = $_SESSION['user_role_id'];
-    $this->entityManager = EntityManagerFactory::getEntityManager();
-  }
+    /**
+     * BaseController constructor.
+     */
+    public function __construct() {
+        if (isset($_SESSION['user_id'])) $this->user_id = $_SESSION['user_id'];
+        if (isset($_SESSION['username'])) $this->username = $_SESSION['username'];
+        if (isset($_SESSION['user_role_id'])) $this->user_role_id = $_SESSION['user_role_id'];
+        $this->entityManager = EntityManagerFactory::getEntityManager();
 
-  /**
-   * Redirect the user to the login page if they are not logged in.
-   *
-   * @return void
-   */
-  public function isUserNotLoggedIn() {
-    if ($this->username === NULL)  {
-      header("Location: /login");
+        // Set up Twig.
+        $loader = new FilesystemLoader(__DIR__ . '/../../templates'); // adjust to your actual path
+        $this->twig = new Environment($loader, [
+            'cache' => false, // or use a path like __DIR__ . '/../../cache/twig' for caching
+            'debug' => true,
+        ]);
     }
-  }
+
+    /**
+     * Render a Twig template.
+     *
+     * @param string $template
+     *   Template file (e.g., 'home/index.html.twig')
+     * @param array $context
+     *   Associative array of variables to pass
+     */
+    public function render(string $template, array $context = []) {
+        echo $this->twig->render($template, $context);
+    }
+
+    /**
+     * Redirect the user to the login page if they are not logged in.
+     *
+     * @return void
+     */
+    public function isUserNotLoggedIn() {
+        if ($this->username === NULL)  {
+            header("Location: /login");
+        }
+    }
 
 }
