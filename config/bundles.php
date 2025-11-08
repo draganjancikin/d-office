5a2440b71b48d8dbe0090acf98c2ea9b7388d47a
Dragan Jancikin <dragan.jancikin@gmail.com>
2025-11-08 10:51:41 +0100

    #351 - Enable TwigBundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-//    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     // Add more bundles here as needed
 ];
 
