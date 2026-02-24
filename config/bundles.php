7cc4a2f76e31bba35992792988e3f44e37520c6c
Dragan Jancikin <dragan.jancikin@gmail.com>
2026-02-24 09:04:35 +0100

    #505 - Update MakerBundle environment in bundles.php (local => dev)

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['local' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
